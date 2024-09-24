a1abb5f962ca024202136e91054c4a3ccd04509d
Dom-Taing <chonodom@gmail.com>
2024-09-23 17:22:40 -0700

    shrink sokPao signature

--- src/utils/createPdf.ts
+++ src/utils/createPdf.ts
@@ -287,8 +287,8 @@ export const createPrescriptionPdf = async (
         }
       );
     } else if (doctor?.name === "Sok Pao") {
-      const signatureWidth = 100;
-      xPos = doc.page.width - 27 - signatureWidth;
+      const signatureWidth = 90;
+      xPos = doc.page.width - 32 - signatureWidth;
       yPos = doc.y;
       doc.image(
         path.resolve(`./public/signature/${getFileName(doctor?.name)}.png`),
