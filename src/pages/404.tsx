218d02497f12905faeb3d410ca32d7b8ede7e527
Dom-Taing <chonodom@gmail.com>
2025-06-16 22:28:52 -0700

    fix build

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 // filepath: /src/pages/404.tsx
 export default function Custom404() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-      <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
+      <Link href="/" style={{ color: "blue", textDecoration: "underline" }}>
         Go back to Home
-      </a>
+      </Link>
     </div>
   );
 }
