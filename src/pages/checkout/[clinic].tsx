a2d170e7a7356cbf2eb028c117e3a6603636fdcd
Dom-Taing <chonodom@gmail.com>
2025-06-18 17:21:30 -0700

    update checkout location

--- src/pages/checkout/[clinic].tsx
+++ src/pages/checkout/[clinic].tsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { createClient } from "@supabase/supabase-js";
 import calculateDistance from "@/utils/calculateDistance";
+import { createSupaClient } from "@/service/supa";
+import { GetServerSideProps } from "next";
+import { getCookieValue } from "@/utils/parseCookie";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "YOUR_SUPABASE_URL";
 const supabaseKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "YOUR_SUPABASE_ANON_KEY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default function CheckOut() {
+interface CheckOutProps {
+  clinicLocation: string;
+}
+
+export default function CheckOut({ clinicLocation }: CheckOutProps) {
   const router = useRouter();
   const [userName, setUserName] = useState("");
   const [loading, setLoading] = useState(true);
@@ -38,12 +45,13 @@ export default function CheckOut() {
           }
         );
         const { latitude, longitude } = position.coords;
+        const clinicCoords = clinicLocation.split(",").map(Number);
         // Calculate the distance to the target location
         const distance = calculateDistance(
           latitude,
           longitude,
-          47.6256242,
-          -122.3571215
+          clinicCoords[0],
+          clinicCoords[1]
         ); // Example coordinates for a location
 
         if (distance >= 100) {
@@ -63,15 +71,15 @@ export default function CheckOut() {
         setUserName(currentUser.name); // Set the authenticated user
 
         // Check if the user has already checked in today
-        const { data: checkInData } = await supabase
+        const { data: checkOutData } = await supabase
           .from("work_time")
           .select("*")
           .eq("user_id", currentUser.id)
           .eq("type", "check_out")
           .gte("time", new Date().toISOString().split("T")[0]); // Check for today's date
 
-        if (checkInData && checkInData.length > 0) {
-          setError("You have already checked in today.");
+        if (checkOutData && checkOutData.length > 0) {
+          setError("You have already checked out today.");
           setLoading(false);
           return;
         }
@@ -119,3 +127,42 @@ export default function CheckOut() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<CheckOutProps> = async (
+  context
+) => {
+  try {
+    const clinicPlus = getCookieValue(context.req.headers.cookie, "clinicPlus");
+    if (clinicPlus !== "ClinicPlus2025!") {
+      return {
+        redirect: {
+          destination: `/password?redirect=${context.resolvedUrl}`,
+          permanent: false,
+        },
+      };
+    }
+    const { clinic } = context.params as { clinic: string };
+    const supabase = createSupaClient();
+
+    let { data: Clinic } = await supabase
+      .from("Clinic")
+      .select("*")
+      .eq("name", clinic);
+    if (!Clinic?.length) {
+      return { redirect: { destination: "/", permanent: false } };
+    }
+
+    return {
+      props: {
+        clinicLocation: Clinic[0].location || "",
+      },
+    };
+  } catch {
+    return {
+      redirect: {
+        destination: `/password?redirect=${context.resolvedUrl}`,
+        permanent: false,
+      },
+    };
+  }
+};
